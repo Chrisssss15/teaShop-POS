c0b08f4b9bb9ea04573a69481ea47415b050f1a2
Chris Tang <1073412@hr.nl>
2026-09-02 17:38:05 +0200

    install Capacitor en IOS ready maken

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bluecup.ordering',
+  appName: 'Blue Cup',
+  webDir: 'dist'
+};
+
+export default config;
